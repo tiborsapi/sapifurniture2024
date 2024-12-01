9de6e1a6915a980697c92ec287674781b71dd016
test <test@test.com>
2024-12-01 21:25:36 +0200

    Fix end to end problem

--- src/eetest/java/ro/sapientia/furniture/bdt/FurnitureBDDEETest.java
+++ src/eetest/java/ro/sapientia/furniture/bdt/FurnitureBDDEETest.java
@@ -7,10 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		  features = { "src/eetest/resources/ro/sapientia/furniture/bdt/" },
-		  glue = {"ro.sapientia.furniture.bdt.definition" },
+		  features = { "src/eetest/resources/ro/sapientia/furniture/bdt/ee" },
+		  glue = {"ro.sapientia.furniture.bdt.ee.definition" },
 		  publish = false,
 		  dryRun = true)
-public class FurnitureBDDTest {
+public class FurnitureBDDEETest {
 
 }
