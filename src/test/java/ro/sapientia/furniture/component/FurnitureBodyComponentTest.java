e10c541c79def132ee0bdc8bb76fc27c8a436edc
test <test@test.com>
2024-12-01 20:32:41 +0200

    Fix component test execution issue

--- src/test/java/ro/sapientia/furniture/component/FurnitureBodyComponentTest.java
+++ src/test/java/ro/sapientia/furniture/component/FurnitureBodyComponentTest.java
@@ -1,33 +1,27 @@
 package ro.sapientia.furniture.component;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import ro.sapientia.furniture.controller.FurnitureController;
 import ro.sapientia.furniture.model.FurnitureBody;
 import ro.sapientia.furniture.repository.FurnitureBodyRepository;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+@AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:test.properties")
-@WebMvcTest(controllers = FurnitureController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class FurnitureBodyComponentTest {
 
 	@Autowired
@@ -46,7 +40,7 @@ public class FurnitureBodyComponentTest {
 
 		this.mockMvc.perform(get("/furniture/all")).andExpect(status().isOk())
 				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$[0].heigth", is(10)));
+				.andExpect(jsonPath("$[0].heigth", is(20)));
 	}
 
 }
