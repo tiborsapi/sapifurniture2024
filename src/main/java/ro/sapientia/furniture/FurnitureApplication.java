9b7fe0d4d73a54beff0c08232dca2c5812c8f9cd
CaedesAeterna <145425203+CaedesAeterna@users.noreply.github.com>
2024-12-10 20:51:57 +0200

    controller runs too

--- src/main/java/ro/sapientia/furniture/FurnitureApplication.java
+++ src/main/java/ro/sapientia/furniture/FurnitureApplication.java
@@ -3,7 +3,7 @@ package ro.sapientia.furniture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ro.sapientia")
 public class FurnitureApplication {
 
 	public static void main(String[] args) {
