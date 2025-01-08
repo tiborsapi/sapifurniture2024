5d616e2efaf5e5aead7b70a6843664df16aa1044
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-01-08 12:50:36 +0200

    Door model

---
+++ src/main/java/ro/sapientia/furniture/model/Door.java
@@ -0,0 +1,46 @@
+package ro.sapientia.furniture.model;
+
+import javax.persistence.*;
+
+@Entity(name = "door")
+@DiscriminatorValue("DOOR")
+public class Door extends FurnitureBody {
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "material")
+    private String material;
+    
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "has_glass")
+    private boolean hasGlass;
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+    public String getColor() {
+    	return color;
+    }
+    public void setColor(String color) {
+    	this.color = color;
+    }
+
+    public boolean isHasGlass() {
+        return hasGlass;
+    }
+
+    public void setHasGlass(boolean hasGlass) {
+        this.hasGlass = hasGlass;
+    }
+
+    @Override
+    public String toString() {
+        return "Door [id=" + getId() + ", width=" + getWidth() + ", heigth=" + getHeigth() + 
+               ", depth=" + getDepth() + ", material=" + material + "color="+ color +", hasGlass=" + hasGlass + "]";
+    }
+}
