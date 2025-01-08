27d8503790af5af94b84bc04cfe54206fb368e35
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-01-08 13:02:51 +0200

    DoorService in sevice forlder

---
+++ src/main/java/ro/sapientia/furniture/service/DoorService.java
@@ -0,0 +1,37 @@
+package ro.sapientia.furniture.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ro.sapientia.furniture.model.Door;
+import ro.sapientia.furniture.repository.DoorRepository;
+
+@Service
+public class DoorService{
+	private final DoorRepository doorRepository;
+	
+	public DoorService(final DoorRepository doorRepository) {
+		this.doorRepository = doorRepository;
+		
+	}
+	public List<Door> findAllDoorBodies() {
+		return this.doorRepository.findAll();
+	}
+	public Door findDoorById(final Long id) {
+		return this.doorRepository.findDoorById(id);
+	}
+	public Door create(Door door) {
+		return this.doorRepository.saveAndFlush(door);
+	}
+	public Door update(Door door) {
+		return this.doorRepository.saveAndFlush(door);
+	}
+	public void delete(Long id) {
+		this.doorRepository.deleteById(id);
+	}
+	
+	
+
+
+}
