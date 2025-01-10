eec7a916450b1551648505ac9ed963535e38e33a
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-01-10 10:39:55 +0200

    Door model update

--- src/main/java/ro/sapientia/furniture/model/Door.java
+++ src/main/java/ro/sapientia/furniture/model/Door.java
@@ -3,18 +3,20 @@ package ro.sapientia.furniture.model;
 import javax.persistence.*;
 
 @Entity(name = "door")
-@DiscriminatorValue("DOOR")
 public class Door extends FurnitureBody {
     private static final long serialVersionUID = 1L;
 
     @Column(name = "material")
-    private String material;
+    private String material; 
     
     @Column(name = "color")
     private String color;
 
     @Column(name = "has_glass")
     private boolean hasGlass;
+    
+    @Column(name ="numberOfGlassPanels")
+    private int numberOfGlassPanels;
 
     public String getMaterial() {
         return material;
@@ -33,6 +35,12 @@ public class Door extends FurnitureBody {
     public boolean isHasGlass() {
         return hasGlass;
     }
+    public void setNumberOfGlassPanels(int numberOfGlassPanels) {
+    	this.numberOfGlassPanels = numberOfGlassPanels;
+    }
+    public int getNumberOfGlassPanels() {
+    	return numberOfGlassPanels;
+    }
 
     public void setHasGlass(boolean hasGlass) {
         this.hasGlass = hasGlass;
@@ -41,6 +49,6 @@ public class Door extends FurnitureBody {
     @Override
     public String toString() {
         return "Door [id=" + getId() + ", width=" + getWidth() + ", heigth=" + getHeigth() + 
-               ", depth=" + getDepth() + ", material=" + material + "color="+ color +", hasGlass=" + hasGlass + "]";
+               ", depth=" + getDepth() + ", material=" + material + "color="+ color +", hasGlass=" + hasGlass + ", numberOfGlassPanels =" + numberOfGlassPanels +  "]";
     }
 }
