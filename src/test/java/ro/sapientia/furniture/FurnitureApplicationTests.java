a764ffc80ad0af2b98b2937aa6d6cf55d9d3696f
Tibor Kiss <kiss.tibor@ms.sapientia.ro>
2025-10-27 20:15:04 +0000

    Fix schema isssue

--- src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
+++ src/test/java/ro/sapientia/furniture/FurnitureApplicationTests.java
@@ -1,9 +1,13 @@
 package ro.sapientia.furniture;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.ANY)
 class FurnitureApplicationTests {
 
 	@Test
